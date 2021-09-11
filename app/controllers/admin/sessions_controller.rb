ad05f57e30eaece535f5e0b0a96058709152d497
y-n-h-y <upboshi.bigboy@gmail.com>
2021-09-11 14:28:23 +0000

    [add]admin items

---
+++ app/controllers/admin/sessions_controller.rb
@@ -0,0 +1,10 @@
+class Admin::SessionsController < ApplicationController
+  def new
+  end
+
+  def creare
+  end
+
+  def destroy
+  end
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+  
+  belongs_to :customer
+  attachment :image
+  
 end
